62ffd3422b112d9dfafdc15d35ada6bdfd14aa00
Philippe Gadiou <philippe.gadiou@gmail.com>
2019-05-23 11:07:57 +0200

    new db, upgraded liana

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,15 +1,2 @@
 ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
 ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
-
-ForestLiana.integrations = {
-  stripe: {
-    api_key: ENV['STRIPE_SECRET_KEY'],
-    mapping: 'Customer.stripe_id'
-  },
-  mixpanel: {
-    api_key: ENV['MIXPANEL_API_KEY'],
-    api_secret: ENV['MIXPANEL_API_SECRET'],
-    mapping: ['Customer.email'],
-    custom_properties: ['Campaign Source', 'plan', 'tutorial complete'],
-  }
-}
