2f28f83938fe26e990959d50545798c5b416092d
Philippe Gadiou <philippe.gadiou@gmail.com>
2019-05-28 18:51:24 +0200

    search review AB

--- lib/forest_liana/collections/customer_stat.rb
+++ lib/forest_liana/collections/customer_stat.rb
@@ -5,9 +5,7 @@ class Forest::CustomerStat
 
   field :id, type: 'Number', is_read_only: true
   field :email, type: 'String', is_read_only: true
-  field :orders, type: 'String', is_read_only: true
-  field :amount, type: 'Number', is_read_only: true
+  field :orders_count, type: 'String', is_read_only: true
+  field :total_amount, type: 'Number', is_read_only: true
 
-
-  # is_searchable: true
 end
--- lib/forest_liana/controllers/customer_stats_controller.rb
+++ lib/forest_liana/controllers/customer_stats_controller.rb
@@ -3,21 +3,57 @@ class Forest::CustomerStatsController < ForestLiana::ApplicationController
 
   before_action :set_params, only: [:index]
 
-  class CustomerStatSerializer
+  class BaseSerializer
     include JSONAPI::Serializer
 
-    attribute :email
-    attribute :amount
-    attribute :orders
+    def type
+      'customerStat'
+    end
 
+    def format_name(attribute_name)
+      attribute_name.to_s.underscore
+    end
+
+    def unformat_name(attribute_name)
+      attribute_name.to_s.dasherize
+    end
+  end
+
+  class CustomerStatSerializer < BaseSerializer
+    attribute :email
+    attribute :total_amount
+    attribute :orders_count
   end
 
   def index
-    customers_count = Customer.where('email LIKE ?', "%#{@search}%").joins(:orders).group(:id).length
-    customers = Customer.where('email LIKE ?', "%#{@search}%").joins(:orders).group(:id).order('id ASC').limit(@limit).offset(@offset - 1)
-    # binding.pry
-    customer_stats = customers.map { |customer| build_customer_stats(customer) }
-    customer_stats_json = JSONAPI::Serializer.serialize(customer_stats, is_collection: true, meta: {count: customers_count})
+    customers_count = Customer.count_by_sql("
+      SELECT COUNT(*)
+      FROM customers
+      WHERE
+        EXISTS (
+          SELECT *
+          FROM orders
+          WHERE orders.customer_id = customers.id
+        )
+        AND email LIKE '%#{@search}%'
+    ")
+    customer_stats = Customer.find_by_sql("
+      SELECT customers.id,
+        customers.email,
+        count(orders.*) AS orders_count,
+        sum(products.price) AS total_amount,
+        customers.created_at,
+        customers.updated_at
+      FROM customers
+      JOIN orders ON customers.id = orders.customer_id
+      JOIN products ON orders.product_id = products.id
+      WHERE email LIKE '%#{@search}%'
+      GROUP BY customers.id
+      ORDER BY customers.id
+      LIMIT #{@limit}
+      OFFSET #{@offset}
+    ")
+    customer_stats_json = CustomerStatSerializer.serialize(customer_stats, is_collection: true, meta: {count: customers_count})
     render json: customer_stats_json
   end
 
@@ -25,25 +61,13 @@ class Forest::CustomerStatsController < ForestLiana::ApplicationController
 
   def set_params
     @limit = params[:page][:size].to_i
-    @offset = params[:page][:number].to_i
+    @offset = (params[:page][:number].to_i - 1) * @limit
     @search = sanitize_sql_like(params[:search]? params[:search] : "")
   end
 
   def sanitize_sql_like(string, escape_character = "\\")
     pattern = Regexp.union(escape_character, "%", "_")
     string.gsub(pattern) { |x| [escape_character, x].join }
-  end
-
-  def build_customer_stats(customer)
-    order = Order.where(customer_id: customer[:id]).joins(:product)
-    total_amount = order.sum(:price)
-    order_count = order.count(:ref)
-    Forest::CustomerStat.new(
-      id: customer[:id],
-      email: customer[:email],
-      amount: total_amount,
-      orders: order_count,
-    )
   end
 
 end
